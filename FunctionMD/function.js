3b05cb007bb83bb2d6706327e974947ee5758656
wilykun1994 <wilykun1994@gmail.com>
2022-04-09 19:56:58 +0700

    Hentongers

---
+++ FunctionMD/function.js
@@ -0,0 +1,104 @@
+const fetch = require('node-fetch')
+const axios = require('axios');
+const chalk = require('chalk')
+const cheerio = require('cheerio');
+const BodyForm = require('form-data')
+const fs = require('fs')
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
+const getRandom = (ext) => {
+	return `${Math.floor(Math.random() * 10000)}${ext}`
+}
+const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
+const runtime = function(seconds) {
+ seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor(seconds % (3600 * 24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " day, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hours, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minute, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " second") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+const color = (text, color) => {
+  return !color ? chalk.gray(text) : chalk.keyword(color)(text)}
+
+const bgcolor = (text, bgcolor) => {
+ return !bgcolor ? chalk.green(text) : chalk.bgKeyword(bgcolor)(text)}
+
+const ConsoleLog = (text, color) => {
+ return !color ? chalk.yellow('[RIFZA] ') + chalk.green(text) : chalk.yellow('[Rifza ') + chalk.keyword(color)(text)}
+
+function webp2mp4File(path) {
+	return new Promise((resolve, reject) => {
+		 const form = new BodyForm()
+		 form.append('new-image-url', '')
+		 form.append('new-image', fs.createReadStream(path))
+		 axios({
+			  method: 'post',
+			  url: 'https://s6.ezgif.com/webp-to-mp4',
+			  data: form,
+			  headers: {
+				   'Content-Type': `multipart/form-data; boundary=${form._boundary}`
+			  }
+		 }).then(({ data }) => {
+			  const bodyFormThen = new BodyForm()
+			  const $ = cheerio.load(data)
+			  const file = $('input[name="file"]').attr('value')
+			  bodyFormThen.append('file', file)
+			  bodyFormThen.append('convert', "Convert WebP to MP4!")
+			  axios({
+				   method: 'post',
+				   url: 'https://ezgif.com/webp-to-mp4/' + file,
+				   data: bodyFormThen,
+				   headers: {
+						'Content-Type': `multipart/form-data; boundary=${bodyFormThen._boundary}`
+				   }
+			  }).then(({ data }) => {
+				   const $ = cheerio.load(data)
+				   const result = 'https:' + $('div#output > p.outfile > video > source').attr('src')
+				   resolve({
+						status: true,
+						message: "Created By MRHRTZ",
+						result: result
+				   })
+			  }).catch(reject)
+		 }).catch(reject)
+	})
+}
+        
+module.exports = { getBuffer, getRandom, fetchJson, runtime, chalk, color, bgcolor, ConsoleLog, webp2mp4File }
+const LordRifza = require.resolve(__filename)
+fs.watchFile(LordRifza, () => {
+fs.unwatchFile(LordRifza)
+console.log(color(`New! >`, 'yellow'), color(`${__filename}`, 'green'))
+delete require.cache[LordRifza]
+require(LordRifza)})
